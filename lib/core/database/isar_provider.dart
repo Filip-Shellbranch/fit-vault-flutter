727991a1615a14ea7078547b6f09ea2a7a764ce4
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-02-21 14:54:30 +0100

    chore: switch to isar_community for AGP 8.0+ compatibility

--- lib/core/database/isar_provider.dart
+++ lib/core/database/isar_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:isar/isar.dart';
+import 'package:isar_community/isar.dart';
 
 /// Provides the created Isar instance throughout the app.
 /// This value in the provider is overridden in main() once the database is fully opened.

2c06426371ddd310b6c205fd425b8d1bf0b2c998
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-02-21 14:11:51 +0100

    feat: add isar provider using riverpod

---
+++ lib/core/database/isar_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isar/isar.dart';
+
+/// Provides the created Isar instance throughout the app.
+/// This value in the provider is overridden in main() once the database is fully opened.
+final isarProvider = Provider<Isar>((ref) {
+  throw UnimplementedError("Isar has not been intialized yet.");
+});

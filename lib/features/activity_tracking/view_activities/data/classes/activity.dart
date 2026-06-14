1740c6f48e7a66d6d09edf64eb4acc4d94184e24
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-06-14 15:53:32 +0200

    feat: create activity class to easier manage activites

---
+++ lib/features/activity_tracking/view_activities/data/classes/activity.dart
@@ -0,0 +1,22 @@
+import 'package:fit_vault_flutter/features/activity_tracking/workout_tracking/data/classes/workout.dart';
+
+sealed class Activity {
+  final DateTime timestamp;
+  Activity(this.timestamp);
+}
+
+class WorkoutActivity extends Activity {
+  final Workout workout;
+
+  WorkoutActivity(super.timestamp, this.workout);
+
+  WorkoutActivity.fromWorkout(Workout workout)
+    : this(workout.startTime, workout);
+}
+
+//TODO: Create RunActivity
+/*class RunActivity extends Activity {
+  //Workout workout;
+  //WorkoutActivity(this.workout)
+}
+*/

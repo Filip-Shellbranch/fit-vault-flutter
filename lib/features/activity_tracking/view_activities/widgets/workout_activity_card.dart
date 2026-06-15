694a03ecc2962243dc493cab87f2b059d1060709
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-06-15 23:32:26 +0200

    fix: remove unused import

--- lib/features/activity_tracking/view_activities/widgets/workout_activity_card.dart
+++ lib/features/activity_tracking/view_activities/widgets/workout_activity_card.dart
@@ -1,4 +1,3 @@
-import 'package:fit_vault_flutter/core/utils/time_formatting.dart';
 import 'package:fit_vault_flutter/features/activity_tracking/view_activities/data/classes/activity.dart';
 import 'package:fit_vault_flutter/features/activity_tracking/workout_tracking/data/classes/workout.dart';
 import 'package:flutter/material.dart';

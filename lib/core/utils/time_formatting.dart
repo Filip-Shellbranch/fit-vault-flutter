a7076875f45e2ccc48b059b2054126a05d1e3f81
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-07-20 19:23:14 +0200

    feat: remove old formatting function

--- lib/core/utils/time_formatting.dart
+++ lib/core/utils/time_formatting.dart
@@ -4,13 +4,6 @@ String format24h(DateTime time) {
   return "$hours:$minutes";
 }
 
-String formatDurationMinutesSeconds(Duration duration) {
-  String minutes = duration.inMinutes.remainder(60).toString();
-  String seconds = duration.inSeconds.remainder(60).toString().padLeft(2, '0');
-
-  return "$minutes:$seconds";
-}
-
 String formatDurationHMS(Duration duration) {
   String pad(int segment) {
     return segment.toString().padLeft(2, "0");

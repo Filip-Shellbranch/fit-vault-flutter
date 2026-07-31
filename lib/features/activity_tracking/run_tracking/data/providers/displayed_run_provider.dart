f352ce2d659beb5e4ca64cf3b6e60eafce52b118
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-07-31 15:31:43 +0200

    fix: remove debug prints

--- lib/features/activity_tracking/run_tracking/data/providers/displayed_run_provider.dart
+++ lib/features/activity_tracking/run_tracking/data/providers/displayed_run_provider.dart
@@ -1,7 +1,6 @@
 import 'package:fit_vault_flutter/features/activity_tracking/core/providers/current_run_provider.dart';
 import 'package:fit_vault_flutter/features/activity_tracking/run_tracking/data/classes/run.dart';
 import 'package:fit_vault_flutter/features/activity_tracking/view_activities/data/providers/edited_run_provider.dart';
-import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'displayed_run_provider.g.dart';
@@ -10,9 +9,7 @@ part 'displayed_run_provider.g.dart';
 class DisplayedRun extends _$DisplayedRun {
   @override
   Run? build() {
-    debugPrint("updated");
     final editedRun = ref.watch(editedRunProvider);
-    debugPrint(editedRun.toString());
     if (editedRun != null) {
       return editedRun;
     }
